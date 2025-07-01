4bc50edf30975683cc5dc052c2c5692057cd9e76
tscholl44 <tscholl4444@gmail.com>
2025-07-01 14:39:17 -0400

    Create user-login-form.component.ts

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  /**
+   * This is the function responsible for sending the form inputs to the backend
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        // Logic for a successful user login goes here!
+        localStorage.setItem('user', JSON.stringify(result.user));
+        localStorage.setItem('token', result.token);
+        this.dialogRef.close(); // This will close the modal on success!
+        this.snackBar.open('Login successful', 'OK', {
+          duration: 2000,
+        });
+        this.router.navigate(["movies"])
+      },
+      (result) => {
+        this.snackBar.open('Login failed' + result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
