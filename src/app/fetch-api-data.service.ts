a904bd697605b60ae05763e1427220e5c5b22d93
tscholl44 <tscholl4444@gmail.com>
2025-07-03 11:22:09 -0400

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'YOUR_HOSTED_API_URL_HERE/';
+const apiUrl = 'https://toms-flix-a1bb67bc1c05.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
